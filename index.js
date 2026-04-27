a37518babf5eedace2255a268eced07d77e82c39
Abdelghfar Salah <abdelghfarsalah8@gnail.com>
2026-04-27 21:45:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("send-message", (data) => {
+    console.log("Message:", data);
+    io.emit("new-message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
